271527bdbae74da07ee102c347cf2e87d8c6aa57
mostafa779v-sys <mostafa779v@gmail.com>
2026-06-30 18:47:11 +0300

    Update model.py

--- training/model.py
+++ training/model.py
@@ -1,15 +1,22 @@
+import timm
 import torch.nn as nn
-from torchvision.models import efficientnet_b0, EfficientNet_B0_Weights
+
+from config import NUM_CLASSES
+
 
 def create_model():
 
-    model = efficientnet_b0(
-        weights=EfficientNet_B0_Weights.DEFAULT
+    model = timm.create_model(
+        "efficientnet_b0",
+        pretrained=True
     )
 
-    model.classifier[1] = nn.Linear(
-        model.classifier[1].in_features,
-        4
+    model.classifier = nn.Sequential(
+        nn.Dropout(0.2),
+        nn.Linear(
+            model.classifier.in_features,
+            NUM_CLASSES
+        )
     )
 
     return model
