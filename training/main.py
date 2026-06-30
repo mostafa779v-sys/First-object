1896a44c33aec8caabff569ae7c99a565033c36a
mostafa779v-sys <mostafa779v@gmail.com>
2026-06-30 17:06:43 +0300

    Update main.py

--- training/main.py
+++ training/main.py
@@ -1,1 +1,19 @@
+from config import *
+from model import create_model
 
+def main():
+
+    print("="*40)
+    print("BrainVisionAI")
+    print("="*40)
+
+    print(f"Device: {DEVICE}")
+    print(f"Image Size: {IMAGE_SIZE}")
+    print(f"Classes: {CLASS_NAMES}")
+
+    model = create_model()
+
+    print("\nModel Loaded Successfully!")
+
+if __name__ == "__main__":
+    main()
